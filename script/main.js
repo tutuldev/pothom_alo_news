d2a66c71544fb29fd41fac6450f1b31c279ef12d
MD ABDUR RAHAMAN <abdurrahaman444.spy.com>
2024-10-21 21:24:53 +0600

    navbar fixed issue

--- script/main.js
+++ script/main.js
@@ -36,4 +36,27 @@ window.addEventListener('resize', updateDimensions);
 
 // display size debugger end
 
+//menu
+// menu bar 
+
+const navbar = document.getElementById('navbar');
+const navLink = document.querySelector('.nav-link');
+const toggleButton = document.getElementById('menu-button');
+
+toggleButton.addEventListener('click', function(event) {
+    navLink.classList.toggle('hidden');
+    // alert (23);
+    event.stopPropagation(); // Prevent click from bubbling up
+});
+
+document.addEventListener('click', function(event) {
+    if (!navbar.contains(event.target)) {
+        navLink.classList.add('hidden'); 
+    }
+});
+
+
+// menu bar end
+// menu end 
+
 
